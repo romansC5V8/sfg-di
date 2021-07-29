444d172f926ed159626070d7344c6a3abe48faf4
Santiago <sanandres_23@yahoo.com>
2021-07-29 23:12:55 +0200

    Qualifier für Serviceunterscheidung

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,7 +10,8 @@ public class ConstructorInjectedController {
 	private final GreetingService greetingService;
 
 	// Bei der Konstruktor-Variante @Autowired ist optional ab Spring 4.2
-	public ConstructorInjectedController(GreetingService greetingService) {
+	// Mit @Qualifier(<lowerCaseBeanName>) kann man genauer sagen, welche Instanz man möchte, da man mit der Interface arbeitet
+	public ConstructorInjectedController(@Qualifier("constructorInjectedGreetingService") GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
 
